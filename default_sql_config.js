8354349ece840828f7ddc1075c7cbcf6227a7991
Shantanu Moghe <mogheshantanu@gmail.com>
2024-01-24 22:29:36 +0530

    default config for data base.

---
+++ default_sql_config.js
@@ -0,0 +1,9 @@
+//add your default config here before building code as .env file is not included.
+const defaultSQLConfig = {
+DB_USER:'sa',
+DB_PASSWORD: '<Your_SA_Password>',
+DB_SERVER : 'sql-server',
+DB_DATABASE : 'ShopDB'
+}
+
+module.exports = defaultSQLConfig
