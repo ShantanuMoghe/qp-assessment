43e27ade073df76bf0f8972c92a9021d5c2fa8cd
Shantanu Moghe <mogheshantanu@gmail.com>
2024-01-24 22:29:59 +0530

    added sql config

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const defaulConfig = require('./default_sql_config');
+
 const sqlConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER || defaulConfig.DB_USER,
+  password: process.env.DB_PASSWORD ||  defaulConfig.DB_PASSWORD,
+  server: process.env.DB_SERVER || defaulConfig.DB_SERVER,
+  database: process.env.DB_DATABASE || defaulConfig.DB_DATABASE,
   options: {
     encrypt: false,
     enableArithAbort: true,
